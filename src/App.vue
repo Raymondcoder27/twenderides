4c1c4e7f15de952dea3ac6a6e5a19772e94b7cd4
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-18 17:25:50 +0300

    adds transaction id to admin agent

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,30 @@ import {useRoute } from 'vue-router';
 import router from '@/router';
 import Home from '@/views/Home.vue';
 import {title} from 'process';
+
+const name = ref<string>("Raymond");
+const age = ref<number>(25);
+
+const route = useRoute();
+
+type SideMenuLink  = {
+  name: string;
+  label: string;
+}
+
+const SideMenu: Ref<Array<SideMenuLink>> = ref([
+  {name: 'home', label: 'Home'},
+  {name: 'about', label: 'About'},
+  {name: 'contact', label: 'Contact'},
+]);
+
+function isRouteActive(routeName: string): {
+  return route.name === routeName;
+}
+
+function navigate(routeName: string) {
+  router.push({name: routeName})
+}
 </script>
 
 <template>
