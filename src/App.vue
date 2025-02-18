4693eba2a1010990a50f7b6fef201d6c8bd09cab
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-18 17:19:44 +0300

    adds transaction id

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import {ref, type Ref } from 'vue';
 import {useRoute } from 'vue-router';
-import router from '@/index';
+import router from '@/router';
+import Home from '@/views/Home.vue';
+import {title} from 'process';
 </script>
 
 <template>
