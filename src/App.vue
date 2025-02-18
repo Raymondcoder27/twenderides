2c5b0e7f5f829ef5c29cfe255db6efc0b01cc4bc
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-18 17:39:29 +0300

    adds transaction id to admin agent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, type Ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
 import router from "@/router";
 import Home from "@/views/Home.vue";
 import { title } from "process";
